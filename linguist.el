43f6705f5e033dce6e32b23e2c6ccc24e61119a2
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-01 14:57:07 +0100

    Add yaml-mode

--- linguist.el
+++ linguist.el
@@ -1,3 +1,7 @@
 (defun linguist--loaded-message (mode)
   "Print a message when a MODE is loaded."
   (message (concat "Linguist: Loaded " mode ".")))
+
+(use-package yaml-mode
+  :config (linguist--loaded-message "yaml-mode")
+  :mode ("\\.\\(e?ya?\\|ra\\)ml\\'" . yaml-mode))
