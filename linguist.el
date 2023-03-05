c3b627d99b2de1a5974182e9e6b2d7ac6ff09786
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-05 13:24:20 +0100

    Add dockerfile-mode

--- linguist.el
+++ linguist.el
@@ -2,6 +2,11 @@
   "Print a message when a MODE is loaded."
   (message (concat "Linguist: Loaded " mode ".")))
 
+(use-package dockerfile-mode
+  :config (linguist--loaded-message "dockerfile-mode")
+  :mode ("\\.dockerfile\\'" . dockerfile-mode)
+  :mode ("[/\\]\\(?:Containerfile\\|Dockerfile\\)\\(?:\\.[^/\\]*\\)?\\'" . dockerfile-mode))
+
 (use-package elixir-mode
   :config (linguist--loaded-message "elixir-mode")
   :mode ("mix\\.lock" . elixir-mode)
