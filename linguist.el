jeffkreeftmeijer/.emacs.d

Add gitignore-mode

linguist.el
@@ -8,6 +8,12 @@
   :mode ("\\.ex\\'" . elixir-mode)
   :mode ("\\.elixir\\'" . elixir-mode))
 
+(use-package git-modes
+  :config (linguist--loaded-message "linguist-mode")
+  :mode ("/git/ignore\\'" . gitignore-mode)
+  :mode ("/info/exclude\\'" . gitignore-mode)
+  :mode ("/\\.gitignore\\'" . gitignore-mode))
+
 (use-package ledger-mode
   :config (linguist--loaded-message "ledger-mode")
   :mode ("\\.ledger\\'" . ledger-mode))

jeffkreeftmeijer/linguist.el

linguist.el
@@ -9,6 +9,12 @@
   :mode ("\\.ex\\'" . elixir-mode)
   :mode ("\\.elixir\\'" . elixir-mode))
 
+(use-package git-modes
+  :config (linguist--loaded-message "gitignore-mode")
+  :mode ("/git/ignore\\'" . gitignore-mode)
+  :mode ("/info/exclude\\'" . gitignore-mode)
+  :mode ("/\\.gitignore\\'" . gitignore-mode))
+
 (use-package ledger-mode
   :config (linguist--loaded-message "ledger-mode")
   :mode ("\\.ledger\\'" . ledger-mode))